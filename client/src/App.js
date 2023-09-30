6dc79366f843940d1e7889601257dccf6acebce1
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-09-30 16:35:46 +0200

    add material UI library

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+function App() {
+    return (
+        <>
+            <Container>
+                Hello World!
+            </Container>
+        </>
+    )
+}
+
+export default App;
