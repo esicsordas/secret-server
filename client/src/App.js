5090e87482502d972f54091f8c13ee1fe2ac6308
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-10-01 10:39:51 +0200

    add navigation to buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import { Button, Container, Typography } from '@mui/material';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import { useNavigate } from 'react-router-dom';
 
 const App = () => {
+    const navigate = useNavigate();
+
+    const goToNewSecret = () => {
+        navigate("/add-new-secret")
+    }
+
+    const goToGetSecret = () => {
+        navigate("/get-a-secret")
+    }
+
     return (
         <Container>
             <Header></Header>
@@ -10,11 +21,11 @@ const App = () => {
             <Typography variant="h2">The Secret Server</Typography>
 
             <Typography>Save your secret, and send to whoever you want to
-             - with the code we provide, only those people can acces the secret, who you want.</Typography>
+             - with the page we provide, only those people can acces the secret, who you want.</Typography>
 
-             <Button variant="contained">Add new secret</Button>
+             <Button variant="contained" onClick={goToNewSecret}>Add new secret</Button>
 
-             <Button variant="contained">Retrieve secret</Button>
+             <Button variant="contained" onClick={goToGetSecret}>Retrieve secret</Button>
 
             <Footer></Footer>
         </Container>
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,16 @@
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/")
+    }
+
     return (
         <>
-            <Button variant="contained">HOME</Button>
+            <Button variant="contained" onClick={handleClick}>HOME</Button>
         </>
     )
 }
