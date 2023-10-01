1c5a3d0029d7945dacb173f882859fdd04f58eb3
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-10-01 18:37:37 +0200

    add back button to end of "new secret"-route

--- client/src/Components/HashCodeProvider.jsx
+++ client/src/Components/HashCodeProvider.jsx
@@ -1,11 +1,18 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const HashCodeProvider = ( { id }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/")
+    }
 
     return (
         <Paper>
             <Typography>Your secret's identifier</Typography>
             <Typography>{id}</Typography>
+            <Button variant="contained" onClick={handleClick}>OK</Button>
         </Paper>
     )
 }
