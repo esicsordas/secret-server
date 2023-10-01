23fe57fc556df0386e44dacecc9f0b53788e9473
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-10-01 19:11:12 +0200

    add behavior to "get-secret" route

--- client/src/Components/HashForm.jsx
+++ client/src/Components/HashForm.jsx
@@ -1,19 +1,60 @@
-import { Paper, TextField, Typography, Button } from "@mui/material";
+import { Paper, InputLabel, Input, Button, FormControl } from "@mui/material";
+import { useState } from "react";
 import SecretProvider from "./SecretProvider";
 
+const getSecret = (id) => {
+  return fetch(`http://localhost:3000/v1/secret/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
 const HashForm = ({ handleChange }) => {
+  const [hashCode, setHashCode] = useState(null);
 
-const handleSubmit = () => {
-    handleChange(<SecretProvider secret={"secret provided from get method"}/>)
-}
+  const handleInputChange = (e) => {
+    setHashCode(e.target.value);
+  };
 
-    return (
-    <Paper>
-        <Typography>Write in the identifier!</Typography>
-        <TextField placeholder="Your identifier"></TextField>
-        <Button variant="contained" onClick={handleSubmit} >SUBMIT</Button>
+  const handleSubmit = (id) => {
+    getSecret(id)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Network response wasn't ok");
+        }
+      })
+      .then((data) => {
+        handleChange(<SecretProvider secret={data} />);
+      })
+      .catch((error) => {
+        console.error("Fetch error: ", error);
+      });
+  };
+
+  return (
+    <Paper sx={{ p: 2, m: 2 }} elevation={4}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(hashCode);
+        }}
+      >
+        <FormControl sx={{ display: "flex", gap: 2 }}>
+          <InputLabel>Write in the identifier!</InputLabel>
+          <Input
+            required={true}
+            onChange={handleInputChange}
+            placeholder="Your identifier"
+          ></Input>
+        </FormControl>
+        <Button type="submit" variant="contained">SUBMIT</Button>
+      </form>
     </Paper>
-)
-}
+  );
+};
 
 export default HashForm;
--- client/src/Components/SecretProvider.jsx
+++ client/src/Components/SecretProvider.jsx
@@ -1,11 +1,18 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const SecretProvider = ( { secret }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/")
+    }
 
 return (
     <Paper>
         <Typography>Your secret:</Typography>
-        <Typography>{secret}</Typography>
+        <Typography>{secret.secret_text}</Typography>
+        <Button variant="contained" onClick={handleClick}>OK</Button>
     </Paper>
 )
 }
