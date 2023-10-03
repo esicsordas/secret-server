08f8a63343a24386cbcc916396b823e4cd5c35b2
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-10-03 09:14:52 +0200

    add back button to errorpage

--- client/src/Pages/ErrorPage.jsx
+++ client/src/Pages/ErrorPage.jsx
@@ -1,5 +1,5 @@
-import { Container, Typography, Paper } from"@mui/material";
-import { useRouteError } from "react-router-dom";
+import { Container, Typography, Paper, Button } from"@mui/material";
+import { useRouteError, useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import { useError } from "../Context/ErrorContext";
@@ -8,7 +8,11 @@ import { useError } from "../Context/ErrorContext";
 const ErrorPage = ( ) => {
     const routeError = useRouteError();
     const { error } = useError();
+    const navigate = useNavigate();
 
+    const handleClick = () => {
+        navigate("/")
+    }
 
     return (
     <Container sx={{ mt: 12, display:"flex", flexDirection:"column", alignItems:"center" }}>
@@ -16,6 +20,7 @@ const ErrorPage = ( ) => {
         <Paper sx={{ px: 4, py: 5, m: 2 , display:"flex", flexDirection:"column", alignItems:"center"}} elevation={4}>
         <Typography variant="h4" sx={{p: 5, color: "primary.error"}}>Sorry, an error occured:</Typography>
         <Typography variant="h5" sx={{p: 5}}>{  error || routeError?.statusText || routeError?.message }</Typography>
+        <Button variant="contained" sx={{ p: 1, m: 2 }} size="large" onClick={handleClick}>BACK TO HOME</Button>
         </Paper>
         <Footer />
     </Container>
